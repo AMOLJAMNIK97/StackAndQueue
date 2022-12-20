da211b1bda88a7713d55729f6b4ecb2917db8053
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-20 18:09:46 +0530

    [amol]- Creat a Stack 56 is in Top position

--- StackandQueueProblem/StackandQueueProblem/Program.cs
+++ StackandQueueProblem/StackandQueueProblem/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using StackandQueueProblem;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack_Ex stack_ = new Stack_Ex();
+        stack_.push(70);
+        stack_.push(30);
+        stack_.push(56);
+       
+    }
+}
---
+++ StackandQueueProblem/StackandQueueProblem/Stack_Ex.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackandQueueProblem
+{
+    internal class Stack_Ex
+    {
+        public class Node
+        {
+            public int data;
+            public Node Next;
+            public Node(int data)
+            {
+                this.data = data;
+                Next = null;
+            }
+        }
+        //top--push--insert, pop---delete,peek--it will returen top element
+        Node top;
+        public Stack_Ex()
+        {
+            this.top = null;
+        }
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.Next = null;
+            }
+            else
+            {
+                node.Next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", data);
+        }
+    }
+}
